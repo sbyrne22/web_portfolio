dfaac81809343766fe95d9c4b901ee0221456c21
Sean Byrne <spickles@Seans-iMac.local>
2018-01-30 17:07:30 -0500

    Site is responsive and updated projCards

--- js/app.js
+++ js/app.js
@@ -2,9 +2,58 @@ $( () => {
 
 // Variables
 
-const $headerBtn = $('.burger')
+const $homeBtn = $('#homeBtn');
+const $headerBtn = $('.burger');
 const $closeheaderBtn = $('.closeheaderBtn');
-const $mobileLink = $('.mobileLink');
+const $mobileLink = $('.mobileLinks');
+
+const $ticTacToes = $('#tic_tac_toes');
+const $tictacToesInfo = $('#ticInfo');
+
+const $appitite = $('#appitite');
+const $appititeInfo = $('#appititeInfo');
+
+const $quip_pro_quo = $('#quip_pro_quo');
+const $quipInfo = $('#quipInfo');
+
+const $xcursion = $('#xcursion');
+const $xcursionInfo = $('#xcursionInfo');
+
+// tic_tac_toes
+$ticTacToes.on( "mouseover", () => {
+  $('#ticInfo').fadeIn( 500 );
+});
+
+$tictacToesInfo.on( "mouseout", () => {
+  $('#ticInfo').fadeOut( 400 );
+});
+
+// Appitite
+$appitite.on( "mouseover", () => {
+  $('#appititeInfo').fadeIn( 500 );
+});
+
+$appititeInfo.on( "mouseout", () => {
+  $('#appititeInfo').fadeOut( 400 );
+});
+
+// quip_pro_quo
+$quip_pro_quo.on( "mouseover", () => {
+  $('#quipInfo').fadeIn( 500 );
+});
+
+$quipInfo.on( "mouseout", () => {
+  $('#quipInfo').fadeOut( 400 );
+});
+
+// xcursion
+$xcursion.on( "mouseover", () => {
+  $('#xcursionInfo').fadeIn( 500 );
+});
+
+$xcursionInfo.on( "mouseout", () => {
+  $('#xcursionInfo').fadeOut( 400 );
+})
 
 // Burger Modal  - - - - - - - -
   $headerBtn.on('click', () => {
@@ -21,16 +70,25 @@ const $mobileLink = $('.mobileLink');
 
   $mobileLink.on('click', () => {
     $('.mobileLinks').css('display', 'none');
-
+    $('.burger').css('display', 'inline-block');
+    $('.closeheaderBtn').css('display', 'none');
+  });
+  $homeBtn.on('click', () => {
+    $('.mobileLinks').css('display', 'none');
+    $('.burger').css('display', 'inline-block');
     $('.closeheaderBtn').css('display', 'none');
   });
 
-  // if ($(window).width() > 750) {
-  //   $('.mobileLinks').css('display', 'none');
-  //   $('.burger').css('display', 'none');
-  //   $('.closeheaderBtn').css('display', 'none');
-  // } else if ($(window).width() < 750){
-  //   $('.burger').css('display', 'inline-block');
-  // }
+$(window).on('resize', () => {
+  if ($(window).width() > 735) {
+    console.log("window width: ", $(window).width());
+    $('.mobileLinks').css('display', 'none');
+    $('.burger').css('display', 'none');
+    $('.closeheaderBtn').css('display', 'none');
+  } else {
+    $('.burger').css('display', 'inline-block');
+  }
+})
+
 
 });
